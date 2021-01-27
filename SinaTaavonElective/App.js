cf3f471da07744028384e93ae185cf2353876635
SinaTaavon <32191839+SinaTaavon@users.noreply.github.com>
2021-01-27 16:55:55 +0000

    Navigation

--- SinaTaavonElective/App.js
+++ SinaTaavonElective/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
- 
+import { BottomNavigation, Text } from 'react-native-paper';
+
 class HelloWorldApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      routes: [
+        { key: 'home', title: 'Home', icon: 'home', color: '#3F51B5' },
+        { key: 'shops', title: 'Coffe Shops', icon: 'store', color: '#009688' },
+        { key: 'search', title: 'Search', icon: 'magnify', color: '#795548' },
+        { key: 'account', title: 'Account', icon: 'account', color: '#607D8B' },
+      ]
+    }
+
+
+  };
+
+  HomeRoute = () => <Text>Home</Text>;
+
+  ShopsRoute = () => <Text>Coffe Shops</Text>;
+
+  SearchRoute = () => <Text>Search</Text>;
+
+  AccountRoute = () => <Text>Account</Text>;
+
+  onChange = (e) => {
+    this.setState({index: e});
+  }
+
+  renderScene = BottomNavigation.SceneMap({
+    home: this.HomeRoute,
+    shops: this.ShopsRoute,
+    search: this.SearchRoute,
+    account: this.AccountRoute,
+
+  });
+
   render() {
-    return(
-      <View>
-        <Text>Hello World</Text>
-      </View>
+    const { index, routes } = this.state;
+    return (
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={this.onChange}
+        renderScene={this.renderScene}
+      />
     );
   }
 }
- 
- export default HelloWorldApp;
+
+export default HelloWorldApp;
--- SinaTaavonElective/index.js
+++ SinaTaavonElective/index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
+import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import { Provider as PaperProvider } from 'react-native-paper';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+  export default class Main extends Component {
+    render() {
+    return (
+        <PaperProvider>
+          <App />
+        </PaperProvider>
+      );
+  }
+}
+
+AppRegistry.registerComponent(appName, () => Main);
