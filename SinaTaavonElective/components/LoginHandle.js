bdeac23b552f84fa8fe238891639b8e0b05e56d3
SinaTaavon <32191839+SinaTaavon@users.noreply.github.com>
2021-02-02 17:34:40 +0000

    Login handler

--- SinaTaavonElective/components/LoginHandle.js
+++ SinaTaavonElective/components/LoginHandle.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, TextInput, Title, Caption } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 import { Alert, View, StyleSheet } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import SignupPage from './SignupPage';
@@ -8,12 +8,9 @@ import LoginPage from './LoginPage';
 class LoginHandle extends Component {
     constructor(props) {
         super(props);
-        this.state = { username: '', password: '', firstname: '', lastname: '', loginStatus: false }
+        this.state = { loginStatus: false }
     };
 
-    onChangeText = (key, value) => {
-        this.setState({ [key]: value })
-    }
 
     loginPageHandler = () => {
 
@@ -25,42 +22,19 @@ class LoginHandle extends Component {
         this.setState({ loginStatus: false })
     }
 
-    signUp = async () => {
-        const { password, email, firstname, lastname } = this.state
-
-        let payload = { first_name: firstname, last_name: lastname, email: email, password: password }
-
-        return fetch('http://10.0.2.2:3333/api/1.0.0/user', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        })
-            .then((response) => {
-                if (response.status === 201) {
-                    console.log("Successful")
-                } else if (response.status === 400) {
-                    console.log("Invalid validation")
-                } else {
-                    console.log("Error")
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-
-    }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.buttonContainer}>
-                    <Button style={styles.loginButton} mode="outlined" onPress={this.SignupPageHandler}>
+                    <Button style={styles.loginButton} compact="true" mode="outlined" onPress={this.SignupPageHandler}>
                         Sign up Page
   </Button>
-                    <Button style={styles.loginButton} mode="outlined" onPress={this.loginPageHandler}>
+                    <Button style={styles.loginButton} compact="true" mode="outlined" onPress={this.loginPageHandler}>
                         Login Page
+  </Button>
+                    <Button style={styles.loginButton} compact="true" mode="outlined" onPress={this.loginPageHandler}>
+                        Clear Session
   </Button>
                 </View>
 
@@ -78,7 +52,7 @@ const styles = StyleSheet.create({
     },
     loginButton: {
         marginTop: 20,
-        marginHorizontal: 10
+        marginHorizontal: 4
     },
     container: {
         flex: 1,
